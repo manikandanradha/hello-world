9b3e6dba4e1df41d6379b99ed9445c05518a3d8b
Manikandan Radha <manikandanradha15@gmail.com>
2023-10-28 19:21:35 +0530

    added 4th file

--- cicd/seedJob.groovy
+++ cicd/seedJob.groovy
@@ -1,16 +1,19 @@
+def mainFolderName = "manikandan"
 def folderName = "hello-world"
 def pipelineDetails = [
   [name: "01-echo-helloworld", script_path: "cicd/01-Jenkinsfile-helloworld"],
   [name: "02-maven-build", script_path: "cicd/02-Jenkinsfile-maven-build"],
   [name: "03-maven-build-tools", script_path: "cicd/03-Jenkinsfile-maven-build-tools"],
+  [name: "04-maven-triggers-hook",script_path: "cicd/04-Jenkinsfile-maven-triggers-hook"]
 ]
 
-folder(folderName) {
+folder(mainFolderName) {
   displayName(folderName)
+  folder(folderName) {}
 }
 
 for (pipelineDetail in pipelineDetails) {
-  pipelineJob("${folderName}/${pipelineDetail.name}") {
+  pipelineJob("${mainFolderName}/${folderName}/${pipelineDetail.name}") {
     definition {
       cpsScm {
         scm{
